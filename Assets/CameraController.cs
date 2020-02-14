3f789fc14bdb99af31eb1bad26d46c460042d0c8
Michael Mederos <megamikers@yahoo.com>
2020-02-13 19:14:41 -0500

    Actual game commit
    Removed messed up game and replaced with actual game project

---
+++ Assets/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public Vector3 offset;
+	public Transform target;
+
+	private float currentZoom = 10f;
+	private float currentSpin = 0f;
+
+	public float zoomInSpeed = 3f;
+	public float spinSpeed = 75f;
+	public float minZoom = 6.5f;
+	public float maxZoom = 15f;
+	public float pitch = 2f;
+
+	void Update () 
+	{
+		currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomInSpeed; // zoom in and out with the scroll wheel
+		currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+		currentSpin -= Input.GetAxis("Horizontal") * spinSpeed * Time.deltaTime;
+	}
+
+    void LateUpdate()
+    {
+		transform.position = target.position - offset * currentZoom; //Set camera position
+		transform.LookAt(target.position + Vector3.up * pitch);
+		transform.RotateAround(target.position, Vector3.up, currentSpin);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+	public LayerMask movementMask;
+	Camera cam;
+	PlayerMotor motor;
+
+    void Start()
+    {
+		cam = Camera.main;
+		motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetMouseButtonDown(0))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray, out hit, 100, movementMask))
+			{
+				//Debug.Log ("Hit" + hit.collider.name + " " + hit.point);
+				motor.MoveToPoint(hit.point);
+			}
+		}
+
+		if(Input.GetMouseButtonDown(1))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray, out hit, 100))
+			{
+				//Debug.Log ("Hit" + hit.collider.name + " " + hit.point);
+				//Is there an interactable object?
+				//Set Player's attentionto interactable object
+			}
+		}
+	}
+}
